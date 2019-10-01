82720160da693eee5d5efea28441132b92ed4dd6
B533P-14\533 <olivier.allaire00@outlook.fr>
2019-10-01 10:10:41 -0400

    Exemple de l'avion

---
+++ examen/Assets/AirPlane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPlane : MonoBehaviour {
+    Rigidbody rbd;
+    public int TrustFACTOR = 5000;
+    public int RelationFactor = 5000;
+    public ParticleSystem Fume;
+    RotationÉlice MonÉlice;
+    // Use this for initialization
+    void Start () {
+        rbd = gameObject.GetComponent<Rigidbody>();
+        MonÉlice = gameObject.GetComponentInChildren<RotationÉlice>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MonÉlice.SpeedRotation(rbd.velocity.magnitude);
+        if (Input.GetAxis("Jump") != 0)
+        {
+            Fume.Play();
+            rbd.AddRelativeForce(new Vector3(Input.GetAxis("Jump") * TrustFACTOR * Time.deltaTime, 0, 0));
+        }
+        else
+        {
+            Fume.Stop();
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+
+            rbd.AddForceAtPosition(new Vector3(Input.GetAxis("Horizontal") * RelationFactor, 0, 0), new Vector3(0, 50, 0));
+        }
+    }
+}
---
+++ examen/Assets/RotationÉlice.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationÉlice : MonoBehaviour {
+
+
+    private float RotationSpeed;
+
+    public void SpeedRotation(float Speed)
+    {
+        RotationSpeed = Speed;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(new Vector3(3,0,0), RotationSpeed * 200);
+    }
+
+    
+}
